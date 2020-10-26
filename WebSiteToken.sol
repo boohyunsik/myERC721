7c6de5c776b7ca47eb1393c3b9129e23967b6752
Hyunsik Boo <bhs9194@nate.com>
2020-10-26 22:41:43 +0900

    Create WebSiteToken.sol

---
+++ WebSiteToken.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "myERC721.sol";
+
+contract WebSiteToken is MyERC721 {
+    
+    mapping(uint256 => string) values;
+    mapping(string => uint256) uriMap;
+    
+    constructor() MyERC721("WebSiteToken", "WST") public {
+        mint(msg.sender, "google.com");
+        mint(msg.sender, "naver.com");
+        mint(msg.sender, "facebook.com");
+    }
+    
+    function mint(address to, string memory uri) public {
+        uint256 newTokenId = totalSupply() + 1;
+        _mint(to, newTokenId);
+        
+        values[newTokenId] = uri;
+        uriMap[uri] = newTokenId;
+    }
+    
+    function getUri(uint256 tokenId) public view returns (string memory) {
+        return values[tokenId];
+    }
+    
+    function getTokenIdByUri(string memory uri) public view returns (uint256) {
+        return uriMap[uri];
+    }
+    
+    function whoHasUri(string memory uri) public view returns (address) {
+        uint256 tokenId = getTokenIdByUri(uri);
+        
+        return ownerOf(tokenId);
+    }
+ 
+}
