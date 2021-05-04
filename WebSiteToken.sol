eda2452d0709fee26a27fc8ea00fd14b5844b8bd
Hyunsik Boo <bhs9194@nate.com>
2021-05-04 18:59:31 +0900

    Update WebSiteToken.sol

--- WebSiteToken.sol
+++ WebSiteToken.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
-import "myERC721.sol";
+import "MyERC721.sol";
 
-contract WebSiteToken is MyERC721 {
+contract WebSiteToken is ERC721 {
     
     mapping(uint256 => string) values;
     mapping(string => uint256) uriMap;
+    uint256 totalSupply;
     
-    constructor() MyERC721("WebSiteToken", "WST") public {
+    constructor() ERC721("WebSiteToken", "WST") public {
+        totalSupply = 0;
         mint(msg.sender, "google.com");
         mint(msg.sender, "naver.com");
         mint(msg.sender, "facebook.com");
@@ -17,11 +19,12 @@ contract WebSiteToken is MyERC721 {
     
     function mint(address to, string memory uri) public {
         require(uriMap[uri] == 0);
-        uint256 newTokenId = totalSupply() + 1;
+        uint256 newTokenId = totalSupply + 1;
         _mint(to, newTokenId);
         
         values[newTokenId] = uri;
         uriMap[uri] = newTokenId;
+        totalSupply = totalSupply + 1;
     }
     
     function getUri(uint256 tokenId) public view returns (string memory) {
